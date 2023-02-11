1a282c9b0706ff51bf9cc9fff3bb72ca9f3b20d9
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2023-02-12 10:12:41 +1100

    Added local hosting server

---
+++ localServer/server.py
@@ -0,0 +1,32 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header("Content-type", "application/pdf")
+        self.end_headers()
+        file = open("./mainPdf_1-1.pdf", "rb")
+        self.wfile.write(file.read())
+        # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        # self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        # self.wfile.write(bytes("<body>", "utf-8"))
+        # self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        # self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
