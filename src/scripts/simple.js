957954d1043260c3b5d08a24c0a74c91862eee37
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2023-02-12 10:24:36 +1100

    Finished embedding PDF

--- src/scripts/simple.js
+++ src/scripts/simple.js
@@ -1,6 +1,9 @@
 // simple.js
+
+
+
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-    'src/scripts/pdfjs-3.3.122-dist/build/pdf.js';
+    'src/scripts/pdfjs-3.3.122-dist/build/pdf.worker.js';
 
 
 
@@ -13,14 +16,18 @@ loadingTask.promise.then(
 	function (pdf) {
 		// Load information from the first page.
 		pdf.getPage(1).then(function (page) {
-			var scale = 5;
-			var viewport = page.getViewport(scale);
+			var scale = 10;
+			var viewport = page.getViewport({scale: scale});
+            console.log(viewport);
 
 			// Apply page dimensions to the `<canvas>` element.
 			var canvas = document.getElementById('pdf');
 			var context = canvas.getContext('2d');
 			canvas.height = viewport.height;
 			canvas.width = viewport.width;
+        //    canvas.height = 0.5*window.innerHeight;
+        //    canvas.width = 0.5*window.innerWidth;
+            
 
 			// Render the page into the `<canvas>` element.
 			var renderContext = {
